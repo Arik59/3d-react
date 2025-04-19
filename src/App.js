14f3748672ef7056e8864d1675c0715532fe3119
Arik59 <kruftik203@gmail.com>
2025-04-19 15:03:51 +0300

    3d cube rotation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
+import { createRoot } from 'react-dom/client';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
+import { OrbitControls } from '@react-three/drei';
+
 import React from 'react';
 import './App.css';
 
+
+function RotateBox() {
+  const meshRef = useRef();
+
+  useFrame (() =>{
+    if (meshRef.current){
+      meshRef.current.rotation.x += 0
+      meshRef.current.rotation.y += 0
+    }
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry args={[2,2,2]}/>
+      <meshStandardMaterial color = "red"/>
+   </mesh>
+  );
+}
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>React template</h1>
-      <p>template for react projects</p>
+    <div className="App" id= "canvas-container">
+        <Canvas>
+            <RotateBox />
+            <ambientLight intensity={0.5}/>
+            <directionalLight color={"red"} position={[0,0,20]} />
+            <OrbitControls />
+        </Canvas >
     </div>
   );
 }
 
+createRoot(document.getElementById('root')).render(<App />)
 export default App;
